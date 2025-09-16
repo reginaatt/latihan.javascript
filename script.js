061da648e4830249e3057834ce90ccae543096f1
reginaatt <rattalariswana@gmail.com>
2025-09-16 10:11:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log("Saya suka matcha"); // output : Saya suka matcha
+
+let nama = "Regina";
+let umur = 15;
+let apakahSudahMenikah = false;
+let jumlahAnak; 
+
+console.log(nama); // output: Regina
+console.log(umur); // output: 15
+console.log(apakahSudahMenikah); // output: false
+console.log(jumlahAnak); // output: undefined
+
+// Operator Aritmatika
+
+// Penjumlahan
+let bilangan1 = 5;
+let bilangan2 = 2;
+console.log(bilangan1 + bilangan2); // output: 7
+
+// Pengurangan
+let _bilangan1 = 20;
+let _bilangan2 = 7;
+console.log(_bilangan1 - _bilangan2); // output: 13
+
+// Perkalian
+let $bilangan1 = 3;
+let $bilangan2 = 7;
+console.log($bilangan1 * $bilangan2); // output: 21
+
+// Pembagian
+let bil1 = 36;
+let bil2 = 2;
+console.log(bil1 / bil2); // output: 18
+
+// Eksponen (Pangkat)
+let _bil1 = 3;
+let _bil2 = 3;
+console.log(_bil1 ** _bil2); // output: 27
+
+// Modulus 
+let $bil1 = 11;
+let $bil2 = 3;
+console.log($bil1 % $bil2); // output: 2
+
+// Increment (Tambah 1)
+let bilangan = 8;
+bilangan++;
+console.log(bilangan); // output: 9
+
+// Decrement (Kurang 1)
+let $bilangan = 5;
+$bilangan--;
+console.log($bilangan); // output: 4
